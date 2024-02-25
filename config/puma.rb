58c85d7c04961ff0560a7c5fa6dede871b20b6d1
InnoIbeto <114240719+InnoIbeto@users.noreply.github.com>
2024-02-26 00:38:34 +0100

    configured files to ensure thay can be deployed on render

--- config/puma.rb
+++ config/puma.rb
@@ -33,3 +33,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+workerS ENV.fetch("WEB_CONCURRENCY") { 4 }
